464452aa119c7c0430caa6ec1c2e30ac858a3856
haurenburu <haurenburu@protonmail.com>
2020-07-22 14:05:02 -0300

    person repo

---
+++ src/main/java/com/example/moviedb/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.example.moviedb.repository;
+
+import com.example.moviedb.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
